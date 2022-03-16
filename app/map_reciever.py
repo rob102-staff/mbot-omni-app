500e0a7008f7a32fcd5d750a7a9b7013d44ccd5e
Tristan Goodell <github@tgoodell.com>
2022-03-16 09:59:13 -0400

    Added LCM functionality helper code to get a map and export it in the right format. Still needs to be integrated to the rest of the Webapp and tested on the robot.

--- app/map_reciever.py
+++ app/map_reciever.py
@@ -1,3 +1,56 @@
+import lcm
+from lcmtypes import occupancy_grid_t
+
 #  TODO: receive map over LCM
 #   - Use LCM logplayer to send a map. Subscribe to the appropriate channel
 #   - Encode the map into a format that can be sent over websocket.
+
+def grabMap(event):
+    # Check to confirm that the channel is the right channel.
+    if event.channel=="SLAM_MAP":
+        # If so, grab the data and set it as a occupancy_grid_t LCM type.
+        data=occupancy_grid_t.decode(event.data)
+
+        # Now extract the data from the struct.
+        utime=int(data.utime)
+    
+        origin_x=float(data.origin_x)
+        origin_y=float(data.origin_y)
+    
+        meters_per_cell=int(data.meters_per_cell)
+        width=int(data.width)
+        height=int(data.height)
+        num_cells=int(data.num_cells)
+        
+        cells=data.cells
+
+        # Send it off for further processing.
+        return utime, origin_x, origin_y, meters_per_cell, width, height, num_cells, cells
+
+def mapToProperArray(event):
+    # Check to confirm that the channel is the right channel.
+    if event.channel=="SLAM_MAP":
+        utime, origin_x, origin_y, meters_per_cell, width, height, num_cells, cells=grabMap(event)
+        mapArray=[]
+
+        mapArray.push(origin_x)
+        mapArray.push(origin_y)
+        mapArray.push(width)
+        mapArray.push(height)
+        mapArray.push(meters_per_cell)
+
+        for cell in cells:
+            mapArray.push(cell)
+
+        return mapArray
+
+lc = lcm.LCM()
+subscription = lc.subscribe("EXAMPLE", grabMap)
+
+try:
+    while True:
+        lc.handle()
+except KeyboardInterrupt:
+    pass
+
+lc.unsubscribe(subscription)
