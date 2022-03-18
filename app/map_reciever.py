b3f8b9c68e401f61cec4e8412e52467826b576e4
Tristan Goodell <github@tgoodell.com>
2022-03-18 11:39:29 -0400

    Removed commented code in app/routes.py

--- app/map_reciever.py
+++
@@ -1,3 +0,0 @@
-#  TODO: receive map over LCM
-#   - Use LCM logplayer to send a map. Subscribe to the appropriate channel
-#   - Encode the map into a format that can be sent over websocket.
--- app/routes.py
+++ app/routes.py
@@ -2,21 +2,6 @@ import flask
 from app import app, socket
 import json
 
-"""The routes module stores all the callback functions associated with each
-endpoint that is exposed by the frontend. Add more here as needed (they must
-start with '/app/'). For example, you might want to have a different endpoint
-for drive commands, mapping commands, etc."""
-
-# TODO: Substitute this with socket if needed!
-# @app.route("/app/motor_cmd", methods=['POST'])
-# def motors_cmd():
-#     # This is a dictionary containing the JSON data sent from the frontend.
-#     form_data = flask.request.get_json(force=True)
-#     if flask.request.method == 'POST':
-#         app.logger.info("Got message:", form_data)
-#         return {}, 200
-#     return {}, 200
-
 @socket.on('connect')
 def setup_connection():
     app.logger.info("Successfully connected!")
@@ -27,21 +12,8 @@ def test_message(data):
     app.logger.info(data)
     socket.emit("message", json.dumps({'data':{'type':'server_test', 'server_test_key':'server_test_val'}}))
 
-
-# TODO: Set up Flask SocketIO to send data/images
-#   - import and setup main functions.
-#   - Send a number that increments every 5 seconds. Make sure the socket is not closing.
-#   - Read an image from file, encode it and send it. 
-#   - Now we can setup LCM
-
 @socket.on('map')
 def send_map(data):
-    # if data=="Need Map. Please give.":
-    #     app.logger.info("Request for map has been received.")
-    #     fin = open("testing.map","r")
-    #     maplines = fin.readlines()
-    #     socket.emit("message", json.dumps({maplines}))
-
     app.logger.info("Message received.")
     app.logger.info(data)
 
@@ -54,4 +26,3 @@ def send_map(data):
 
     return maplines
 
-    # return json.dump({'map': [0, 0, 0, 0]}, 2)
