57639a7d3c1415d0170c2b629207a55b678b91c1
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2022-03-30 09:13:52 -0400

    Returns LCM and comments/correction to CORs

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 
 # The Flask app gets created here. Other Python functions and classes should be
 # stored in the "src" folder.
-app = Flask(__name__, static_folder='../../client/build/static')
-app.config['SECRET_KEY'] = 'development key'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'development key'  # TODO: Verify necessity of this key
 socket = SocketIO(app, cors_allowed_origins='*')
-CORS(app)
+CORS(app)  # TODO: Verify necessity of CORS while using localhost
 
 from app import routes
