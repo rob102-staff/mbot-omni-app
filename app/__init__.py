898d773511d6a5c82941fa5ac8d34e61a7b5d9b2
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2022-04-07 15:01:17 -0400

    Adds lcm manager.
    The manager runs a main loop
    Can house handlers with callback functions.
    Houses the publishing functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from app.lcm_message_publishers import LcmCommunicationManager
 
 # The Flask app gets created here. Other Python functions and classes should be
 # stored in the "src" folder.
@@ -8,5 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'development key'
 socket = SocketIO(app , cors_allowed_origins='*')
 CORS(app) 
+
+lcm_manager = LcmCommunicationManager()
 
 from app import routes
--- app/lcm_message_publishers.py
+++ app/lcm_message_publishers.py
@@ -1,18 +1,69 @@
 import time
 import sys
 import lcm
-sys.path.append("../")
-from lcmtypes import omni_motor_command_t
-from lcm_channel_names import MBOT_MOTOR_COMMAND_CHANNEL
+from lcmtypes.omni_motor_command_t import omni_motor_command_t
+from lcmtypes.occupancy_grid_t import occupancy_grid_t
+from app.lcm_settings import LCM_ADDRESS, MBOT_MOTOR_COMMAND_CHANNEL, SLAM_MAP_CHANNEL
+import threading
 
-lcm_instance = lcm.LCM(LCM_ADDRESS)
-def motor_command_publisher(vx, vy, wz):
-    cmd = omni_motor_command_t
-    cmd.vx = vx 
-    cmd.vy = vy 
-    cmd.wz = wz 
-    cmd.utime = time.time()
-    lc.publish("MBOT_MOTOR_COMMAND", path_cmd.encode())
+class LcmCommunicationManager:
+    def __init__(self, callback_dict={}):
+        '''
+        Runs the lcm handler thread
+        
+        :param callback_dict: contains lcm channel names as keys and 
+            callback functions as values. The functions are called when
+            a message on their corresponding channel is handled. The decoded
+            data will be passed to the callback function.
+        '''
+        self._lcm = lcm.LCM(LCM_ADDRESS)
+        self.subscriptions = []
+        self._callback_dict = callback_dict
+        
+        ###################################
+        # TODO: EXAMPLE subscribe to a channel
+        #       Verify and fix!
+        self.__subscribe(SLAM_MAP_CHANNEL, self.example_occupancy_grid_handler)
+        ###################################
 
-# TODO: create such function templates.
-def other_publisher(): raise(Exception("Not Yet Implemented!"))
+        self.__lcm_thread = threading.Thread(target=self.__run_handle_loop)
+        self.__lcm_thread.start()
+
+    def update_callback(self, channel, function):
+        self._callback_dict[channel] = function
+    
+    def __subscribe(self, channel, handler):
+        self.subscriptions.append(self._lcm.subscribe(channel, handler))
+
+    def __run_handle_loop(self): 
+        while True: 
+            self._lcm.handle()
+    
+    def publish_motor_commands(self, vx, vy, wz):
+        cmd = omni_motor_command_t()
+        cmd.vx = vx; cmd.vy = vy; cmd.wz = wz 
+        cmd.utime = int(time.time() * 1000)
+        self._lcm.publish(MBOT_MOTOR_COMMAND_CHANNEL, cmd.encode())
+        print(f"published: {vx}, {vy}, {wz} to the channel {MBOT_MOTOR_COMMAND_CHANNEL}")  # TODO: remove. For testing
+
+    # TODO: create such function templates for every function that 
+    # needs to be implmeneted.
+    def other_publisher(): 
+        raise(Exception("Not Yet Implemented!"))
+
+    # TODO: remove this. Only for testing.
+    def publish_empty_grid(self):  
+        g = occupancy_grid_t()
+        self._lcm.publish(SLAM_MAP_CHANNEL, g.encode())
+
+    # TODO: validate and fix! remove example from the name afterwards.
+    def example_occupancy_grid_handler(self, channel, data):
+        decoded_data = occupancy_grid_t.decode(data)
+        if channel in self._callback_dict.keys(): 
+            self._callback_dict[channel](decoded_data)
+        print("received map!")  # TODO: remove
+
+    def __del__(self):
+        print("joined thread")
+        self.__lcm_thread.join()
+        for s in self.subscriptions: self._lcm.unsubscribe(s)
