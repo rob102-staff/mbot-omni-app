1049c265db82603b130ea1cd893f248ebc58ea35
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2022-04-13 21:40:53 -0400

    Connects backend lcm map to frontend map render.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
-from app.lcm_message_publishers import LcmCommunicationManager
+from app.lcm_manager import LcmCommunicationManager
+from app.lcm_settings import *
+from app.lcm_callbacks import OccupancyGridEmiter
 
 # The Flask app gets created here. Other Python functions and classes should be
 # stored in the "src" folder.
@@ -10,6 +12,11 @@ app.config['SECRET_KEY'] = 'development key'
 socket = SocketIO(app , cors_allowed_origins='*')
 CORS(app) 
 
-lcm_manager = LcmCommunicationManager()
+lcm_callback_dict = {
+    SLAM_MAP_CHANNEL: OccupancyGridEmiter(socket, 'map', period=0.5)
+}
+
+lcm_manager = LcmCommunicationManager(lcm_callback_dict)
+
 
 from app import routes
