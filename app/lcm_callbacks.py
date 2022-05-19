b4c95adfea822a1c3e871694665f3ec47077b2d4
imadhav <imadhav@umich.edu>
2022-05-19 15:26:22 -0400

    initial tests in lidar_testing

--- app/lcm_callbacks.py
+++ app/lcm_callbacks.py
@@ -43,3 +43,46 @@ class OccupancyGridEmiter():
     def __del__(self):
         self.__stop_thread = True
         self.__thread.join()
+
+
+class LidarReader():
+    def __init__(self, socket, event_name, period):
+        self.__socket           = socket
+        self.__event_name       = event_name
+        self.__period           = period
+        self.__lidar_available  = False
+        self.__lidar            = None
+
+        self.__lock   = threading.Lock()
+        self.__thread = threading.Thread(target=self.__run)
+        self.__stop_thread = False
+        self.__thread.start()
+
+    def __lcm_lidar_to_dict(self):
+        return {
+            "utime" : self.__lidar.utime, 
+            "num_ranges" : self.__lidar.num_ranges,
+            "ranges" : self.__lidar.ranges,
+            "thetas" : self.__lidar.width,
+            "times" : self.__lidar.times,
+            "intensities" : self.__lidar.intensities,
+        }
+
+    def __run(self):
+        while not self.__stop_thread:
+            time.sleep(self.__period)
+            if self.__lidar_available:
+                self.__lock.acquire()
+                self.__socket.emit(self.__event_name, self.__lcm_lidar_to_dict())
+                self.__lidar_available = False
+                self.__lock.release()
+
+    def __call__(self, data):
+        self.__lock.acquire()
+        self.__lidar = data
+        self.__lidar_available = True
+        self.__lock.release()
+
+    def __del__(self):
+        self.__stop_thread = True
+        self.__thread.join()
