99b129721648fce14643eb837cf5f0348f674c9e
tgoodell <me@tgoodell.com>
2022-06-21 14:43:30 -0400

    progress on getting camera to work

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,22 @@ import flask
 from app import app, socket, lcm_manager
 import json
 
+#!/usr/bin/env python
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera_opencv import Camera
+
+# Raspberry Pi camera module (requires picamera package)
+from camera_opencv import Camera
+
+app = Flask(__name__)
+
 @socket.on('connect')
 def setup_connection():
     app.logger.info("Successfully connected!")
@@ -11,6 +27,28 @@ def test_message(data):
     app.logger.info("The test message is received!!!")
     app.logger.info(data)
     socket.emit("message", json.dumps({'data':{'type':'server_test', 'server_test_key':'server_test_val'}}))
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    yield b'--frame\r\n'
+    while True:
+        frame = camera.get_frame()
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+
+
+@app.route('/video_feed')
+def video_feed():
+    print("hi how are you")
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @socket.on('map')
 def send_map(data):
@@ -45,4 +83,3 @@ def test_message(data):
     app.logger.info("STOP!!!")
     app.logger.info(data)
     lcm_manager.publish_motor_commands(0,0,0)
-
