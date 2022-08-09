e01d271a8493d8cb20dea75c51efd05a691623cd
Jana <jbpavlasek@gmail.com>
2022-08-09 11:24:31 -0400

    Specify python 3 interpreter for installation

--- install_lcm.sh
+++ install_lcm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Install LCM files.
-PY_INSTALL_DIR=$(python -c "from distutils import sysconfig as sc; print(sc.get_python_lib())")
+PY_INSTALL_DIR=$(python3 -c "from distutils import sysconfig as sc; print(sc.get_python_lib())")
 
 lcm-gen -p --ppath $PY_INSTALL_DIR lcmtypes/*.lcm
